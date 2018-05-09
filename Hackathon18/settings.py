90fddf23feb3d80ea3d2aa73f8942008bf79cbcf
RogerAce <roopam.panwar@gmail.com>
2018-05-09 23:04:27 +0530

    ||

--- Hackathon18/settings.py
+++ Hackathon18/settings.py
@@ -125,5 +125,5 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
